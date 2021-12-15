e851e5451c338c2df46ae7a15eeb7f028aa13af2
zhen <zhen.yang@hotmail.fr>
2021-12-15 16:32:24 +0100

    maj

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,14 @@
 $( document ).ready(function() {
-    
+    resizeAdminHeaderMenu();
+    resizeAdminHeaderMenu2();
 });
+
+function resizeAdminHeaderMenu(){
+
+}
+
+function resizeAdminHeaderMenu2(){
+    window.addEventListener('resize', (event)=>{
+        
+    });
+}
